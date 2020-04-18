b6701cf2fedb77d40d852f4d1306355694a05e3c
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-04-18 14:38:34 -0400

    fianl coding

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,17 +11,25 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generateRandom(array){
+  var randomIndex = Math.floor(Math.random()*array.length);
+  var randomEl = array[randomIndex];
+  return randomEl;
+}
 
 function generatePassword() {
 
   // set up string for password values
-  var lowerCase = ["a","b","c"];
-  var upperCase = ["A","B","C","D","E"];
-  var num = ["1","2"];
-  var speChar = ("!@#$%^&*+-()");
-  var pass = ("");
+
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var num = ["1","2","3","4","5","6","7","8","9"];
+  var speChar = ["!","@","#","$","%","^","&","*","+","-","(",")"];
+  var pass = [];
+  var availChar = [];
 
   // Create prompet for character length and alerts
+
   var passLength = prompt("Length of password? (8-128 characters)");
   if (passLength < 8) {
     alert("Passward cannot be less than 8 characters.");
@@ -31,80 +41,70 @@ function generatePassword() {
   }
 
   // Confirming Lower case
+
   var passLowerCase = confirm("Include lower case letters?");
   if (passLowerCase) {
     alert("You want to include lower case.");
+    availChar = availChar.concat(lowerCase);
+    pass.push(generateRandom(lowerCase));
   }
 
   else {
-    alert("You do not want to include lower case.")
+    alert("You do not want to include lower case.");
 
   }
 
   // Confirming upper case
+
   var passUpperCase = confirm("Include uppercase letters?");
   if (passUpperCase) {
-    alert("You want to include upper case.")
+    alert("You want to include upper case.");
+    availChar = availChar.concat(upperCase);
+    pass.push(generateRandom(upperCase));
 
   }
 
   else {
-    alert("You do not want to include upper case.")
+    alert("You do not want to include upper case.");
 
   }
 
   // Confirming numbers
+
   var passNum = confirm("Include numbers?");
   if (passNum) {
-    alert("You want to include numbers.")
+    alert("You want to include numbers.");
+    availChar = availChar.concat(num);
+    pass.push(generateRandom(num));
   }
 
   else {
-    alert("You do not want to include numbers.")
+    alert("You do not want to include numbers.");
 
   }
 
   // Confirming special characters
+
   var passSpeChar = confirm("Include special characters?");
   if (passSpeChar) {
-    alert("You want to include special characters.")
+    alert("You want to include special characters.");
+    availChar = availChar.concat(speChar);
+    pass.push(generateRandom(speChar));
   }
 
   else {
-    alert("You do not want to include special characters.")
+    alert("You do not want to include special characters.");
 
   }
-
-//create for loop to choose password characters
-
-//for (var i = 0; i < passLength.value; i++){
+ 
+  //create for loop to choose password characters
   //randomly generate
-  if (passLowerCase) {
-    var lowerCaseRan = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    pass = pass + lowerCaseRan;
 
+  for (var i = pass.length; i < passLength; i++){
+    pass.push(generateRandom(availChar));
   }
-
-  if (passUpperCase) {
-    var upperCaseRan = upperCase[Math.floor(Math.random() * upperCase.length)];
-    pass = pass + upperCaseRan;
-
-  }
-
-  if (passNum) {
-    var numRan = num[Math.floor(Math.random() * num.length)];
-    pass = pass + numRan;
-
-  }
-
-  if (passSpeChar) {
-    var specRan = speChar[Math.floor(Math.random() * speChar.length)];
-    pass = pass + specRan;
-
-  }
-//}
-
-  return pass;
+  var randomPass = pass.join("");
+  return randomPass;
 }
 
 // Add event listener to generate button
